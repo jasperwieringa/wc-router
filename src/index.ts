44a8f5d1f621c95f4861bd41aeb099bef2cd6820
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-01 11:06:14 +0200

    Finished setting up the routing for AIR

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,58 @@ import { LitElement, html } from 'lit';
 import { connectRouter } from 'lit-redux-router';
 import { store } from './utils/store.js';
 
+import './components/common/loader/air-loader.js';
+
 connectRouter(store);
 
 class Air extends LitElement {
   render() {
     return html`
+      <div>
+        <a href="/">home</a>
+        <a href="/login">login</a>
+        <a href="/projects">projects</a>
+        <a href="/deliverables">deliverables</a>
+        <a href="/objects">objects</a>
+      </div>
       <div class="app-content">
-        <lit-route path="/" component="air-registration"></lit-route>
-        <lit-route path="/projects" component="air-projects"></lit-route>
+        <lit-route
+          path="/"
+          component="air-core"
+          .resolve="${() => import('./components/air-core.js')}"
+          loading="air-loader"
+        ></lit-route>
+        <lit-route
+          path="/login"
+          component="air-login"
+          .resolve="${() => import('./components/registration/air-login.js')}"
+          loading="air-loader"
+        ></lit-route>
+        <lit-route
+          path="/projects"
+          component="air-projects"
+          .resolve="${() => import('./components/projects/air-projects.js')}"
+          loading="air-loader"
+        ></lit-route>
         <lit-route
           path="/deliverables"
           component="air-deliverables"
+          .resolve="${() =>
+            import('./components/deliverables/air-deliverables.js')}"
+          loading="air-loader"
         ></lit-route>
-        <lit-route path="/objects" component="air-objects"></lit-route>
-        <lit-route path="/report" component="air-report"></lit-route>
+        <lit-route
+          path="/objects"
+          component="air-objects"
+          .resolve="${() => import('./components/objects/air-objects.js')}"
+          loading="air-loader"
+        ></lit-route>
+        <lit-route
+          path="/report"
+          component="air-report"
+          .resolve="${() => import('./components/report/air-report.js')}"
+          loading="air-loader"
+        ></lit-route>
         <lit-route>404</lit-route>
       </div>
     `;
