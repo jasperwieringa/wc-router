fd391d3ea7168e3047e1a89c764a4f94b42f7fb3
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-01 11:14:49 +0200

    Used the AirLoader static tag method to avoid typos

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { LitElement, html } from 'lit';
 import { connectRouter } from 'lit-redux-router';
 import { store } from './utils/store.js';
 
-import './components/common/loader/air-loader.js';
+import { AirLoader } from './components/common/loader/AirLoader.js';
 
 connectRouter(store);
 
@@ -21,38 +21,38 @@ class Air extends LitElement {
           path="/"
           component="air-core"
           .resolve="${() => import('./components/air-core.js')}"
-          loading="air-loader"
+          loading="${AirLoader.tag}"
         ></lit-route>
         <lit-route
           path="/login"
           component="air-login"
           .resolve="${() => import('./components/registration/air-login.js')}"
-          loading="air-loader"
+          loading="${AirLoader.tag}"
         ></lit-route>
         <lit-route
           path="/projects"
           component="air-projects"
           .resolve="${() => import('./components/projects/air-projects.js')}"
-          loading="air-loader"
+          loading="${AirLoader.tag}"
         ></lit-route>
         <lit-route
           path="/deliverables"
           component="air-deliverables"
           .resolve="${() =>
             import('./components/deliverables/air-deliverables.js')}"
-          loading="air-loader"
+          loading="${AirLoader.tag}"
         ></lit-route>
         <lit-route
           path="/objects"
           component="air-objects"
           .resolve="${() => import('./components/objects/air-objects.js')}"
-          loading="air-loader"
+          loading="${AirLoader.tag}"
         ></lit-route>
         <lit-route
           path="/report"
           component="air-report"
           .resolve="${() => import('./components/report/air-report.js')}"
-          loading="air-loader"
+          loading="${AirLoader.tag}"
         ></lit-route>
         <lit-route>404</lit-route>
       </div>
